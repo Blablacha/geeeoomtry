fc570d59391c8d82a574b9ce404116f5a48d87e4
Blablacha <leilablossom@mail.ru>
2023-03-06 13:44:18 +0300

    scale works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -80,6 +80,11 @@ public class Task {
     private static final int DELIMITER_ORDER = 10;
 
     /**
+     * коэффициент колёсика мыши
+     */
+    private static final float WHEEL_SENSITIVE = 0.001f;
+
+    /**
      * Задача
      *
      * @param ownCS  СК задачи
@@ -296,5 +301,19 @@ public class Task {
         renderGrid(canvas, lastWindowCS);
         // рисуем задачу
         renderTask(canvas, windowCS);
+    }
+
+    /**
+     * Масштабирование области просмотра задачи
+     *
+     * @param delta  прокрутка колеса
+     * @param center центр масштабирования
+     */
+    public void scale(float delta, Vector2i center) {
+        if (lastWindowCS == null) return;
+        // получаем координаты центра масштабирования в СК задачи
+        Vector2d realCenter = ownCS.getCoords(center, lastWindowCS);
+        // выполняем масштабирование
+        ownCS.scale(1 + delta * WHEEL_SENSITIVE, realCenter);
     }
 }
